0f01de3c22fab05053c2621af902b15f4eb7dcf6
Diego Marrero Ferrera <diego.marrero104@alu.ulpgc.es>
2023-10-31 12:27:40 +0000

    first commit.

---
+++ src/main/java/marrero_ferrera_gcid_ulpgc/test/control/Main.java
@@ -0,0 +1,17 @@
+package marrero_ferrera_gcid_ulpgc.test.control;
+
+public class Main {
+    public static void main(String[] args) {
+        /*
+        0. Cargar localizaciones
+        1. Crear controladores
+        2. crear tarea periódica
+            2.1 ejecutar tarea (WeatherController -> execute() )
+            CREAR CLASES "WEATHERCONTROLLER Y WEATHERSUPPLIER"
+        3.
+         */
+
+        // DESERIALIZAR CON GSON EL TEXTO DEL API TESTER
+        // JsonObject.class
+    }
+}
---
+++ src/main/java/marrero_ferrera_gcid_ulpgc/test/model/Weather.java
@@ -0,0 +1,59 @@
+package marrero_ferrera_gcid_ulpgc.test.model;
+import java.time.Instant;
+
+public class Weather {
+    private static Instant ts;
+    private float rain;
+    private int clouds;
+    private float temperature;
+    private float humidity;
+    private Location location;
+
+    public Weather(float rain, int clouds, float temperature, float humidity, Location location) {
+        this.rain = rain;
+        this.clouds = clouds;
+        this.temperature = temperature;
+        this.humidity = humidity;
+        this.location = location;
+    }
+
+    public float getRain() {
+        return rain;
+    }
+
+    public void setRain(float rain) {
+        this.rain = rain;
+    }
+
+    public int getClouds() {
+        return clouds;
+    }
+
+    public void setClouds(int clouds) {
+        this.clouds = clouds;
+    }
+
+    public float getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(float temperature) {
+        this.temperature = temperature;
+    }
+
+    public float getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(float humidity) {
+        this.humidity = humidity;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+}
