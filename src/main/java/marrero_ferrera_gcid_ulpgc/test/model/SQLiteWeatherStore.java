cf31ddfecf3de1e3e7cc459ad47f3ebdbaed0d14
Diego Marrero Ferrera <diego.marrero104@alu.ulpgc.es>
2023-11-10 14:32:16 +0000

    First DB code created. ERRORS COMPILATIONS

--- src/main/java/marrero_ferrera_gcid_ulpgc/test/model/SQLiteWeatherStore.java
+++ src/main/java/marrero_ferrera_gcid_ulpgc/test/model/SQLiteWeatherStore.java
@@ -1,4 +1,42 @@
 package marrero_ferrera_gcid_ulpgc.test.model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class SQLiteWeatherStore implements WeatherStore {
+    @Override
+    public void createTable() {
+
+        final String DATABASE_URL = "jdbc:sqlite:./IdeaProjects/practice1_v2/sqlite/java";
+        // C:\Users\dmarr\IdeaProjects\practice1_v2\src\main\java\marrero_ferrera_gcid_ulpgc\test\model
+
+        try (Connection connection = DriverManager.getConnection(DATABASE_URL);
+             PreparedStatement statement = connection.prepareStatement(
+                     "CREATE TABLE IF NOT EXISTS weather_table (" +
+                             "id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                             "location TEXT," +
+                             "temperature REAL" +
+                             // Add other columns as needed
+                             ");")) {
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.getMessage();
+        }
+    }
+
+    @Override
+    public void insertWeather(Weather weather) {
+        try (Connection connection = DriverManager.getConnection(DATABASE_URL);
+             PreparedStatement statement = connection.prepareStatement(
+                     "INSERT INTO weather_table (location, temperature) VALUES (?, ?);")) {
+            statement.setString(1, weather.getLocation());
+            statement.setDouble(2, weather.getTemperature());
+            // Set other parameters as needed
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.getMessage();
+        }
+    }
 }
--- src/main/java/marrero_ferrera_gcid_ulpgc/test/model/WeatherStore.java
+++ src/main/java/marrero_ferrera_gcid_ulpgc/test/model/WeatherStore.java
@@ -1,4 +1,6 @@
 package marrero_ferrera_gcid_ulpgc.test.model;
 
 public interface WeatherStore {
+    void createTable();
+    void insertWeather(Weather weather);
 }
